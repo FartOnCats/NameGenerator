2938f467fe7fc1a483adba49a0c86d470fba115f
FartOnCats <19505519+FartOnCats@users.noreply.github.com>
2018-02-02 11:18:28 -0500

    added some words

--- WordLists.java
+++ WordLists.java
@@ -1,7 +1,10 @@
+/*fartoncats
+* 2/2/2018
+*/
 package main;
 public class WordLists {
 	public  static String[] First = {"pizza","Speghetti","soccer","Minecraft","FootBall","Soup","Boat","BasketBall",
-			"Boxing","Eating"};
+			"Boxing","Eating","Sleeping","Flying","Tennis"};
 	
 	public static String[] Last = {"Larry","Mom","Dad","Soma","Minion","Lover","Player","Gamer","Driver","Greg",
 			"Phill","Early-bird","Eavesdropper","Eccentric","Egghead","Enthusiast","Expert", "Extremist"
